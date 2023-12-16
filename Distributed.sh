62fe3bcf74bd02d67096be696fbd5d9c5385cfc0
taidavis1 <73322981+taidavis1@users.noreply.github.com>
2023-12-15 22:47:14 -0800

    Create Distributed.sh

---
+++ Distributed.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=singlenode
+#SBATCH --gpus-per-node=${NUM_GPUS}
+#SBATCH --nodes=${NUM_NODES}
+
+module purge                        
+ml cudatoolkit-standalone/11.8.0    
+ml tensorflow
+
+
+if [[-n "${NUM_NODES}" && "${NUM_NODES}" == 1]]; then
+    !python /models/research/object_detection/model_main_tf2.py \
+    --pipeline_config_path=/content/faster_rcnn_resnet101_v1_800x1333_coco17_gpu-8/pipeline.config \
+    --model_dir=/content/training \
+    --alsologtostderr
+
+elif [[-n "${NUM_NODES}" && "${NUM_NODES}" > 1]]; then
+    !python /models/research/object_detection/model_main_tf2.py --num_workers=${NUM_NODES} \
+    --pipeline_config_path=/content/faster_rcnn_resnet101_v1_800x1333_coco17_gpu-8/pipeline.config \
+    --model_dir=/content/training \
+    --alsologtostderr
